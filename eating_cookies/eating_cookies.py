86156b6c79e384f538afe6fa6d0a70a7d8173f16
Pav0l <pavol.karas@gmail.com>
2019-06-05 15:49:11 +0200

    Add comments to Cookie Monster

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -13,14 +13,17 @@ def eating_cookies(n, cache=None):
     # base conditions:
     if n <= 0:
         return 1
+    # 1 cookie can be eaten only 1 way
     elif n == 1:
         return 1
+    # 2 cookies can be eaten 2 ways (1+1 or 2)
     elif n == 2:
         return 2
+    # 3 cookies can be eaten 4 ways (1+1+1 or 2+1 or 1+2 or 3)
     elif n == 3:
         return 4
     else:
-        # do recursion
+        # do recursion for every Cookie Monster capability
         return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 
