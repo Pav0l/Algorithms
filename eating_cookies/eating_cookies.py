e458178fef6cb3ab4e9ca903bb73243235a89013
Pav0l <pavol.karas@gmail.com>
2019-06-06 10:26:29 +0200

    Eat cookies with caching

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python
 
 import sys
-
-# The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive
-# recursive solution
-
-# cache={"0": 0, "1": 1, "2": 2, "3": 4}
+from time import time
 
 
-def eating_cookies(n, cache=None):
+# def eating_cookies(n, cache=None):
+#     # base conditions:
+#     if n <= 0:
+#         return 1
+#     # 1 cookie can be eaten only 1 way
+#     elif n == 1:
+#         return 1
+#     # 2 cookies can be eaten 2 ways (1+1 or 2)
+#     elif n == 2:
+#         return 2
+#     # 3 cookies can be eaten 4 ways (1+1+1 or 2+1 or 1+2 or 3)
+#     elif n == 3:
+#         return 4
+#     else:
+#         # do recursion for every Cookie Monster capability
+#         return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
+
+def cache_eating_cookies(n, cache={}):
     # base conditions:
-    if n <= 0:
+    if n < 0:
         return 1
     # 1 cookie can be eaten only 1 way
-    elif n == 1:
+    elif n == 1 or n == 0:
         return 1
     # 2 cookies can be eaten 2 ways (1+1 or 2)
     elif n == 2:
@@ -22,15 +35,23 @@ def eating_cookies(n, cache=None):
     # 3 cookies can be eaten 4 ways (1+1+1 or 2+1 or 1+2 or 3)
     elif n == 3:
         return 4
+    elif str(n) in cache:
+        return cache[str(n)]
     else:
-        # do recursion for every Cookie Monster capability
-        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+        cache[str(n)] = cache_eating_cookies(
+            n-1, cache) + cache_eating_cookies(n-2, cache) + cache_eating_cookies(n-3, cache)
+        return cache[str(n)]
 
+
+# start = time()
+# print(cache_eating_cookies(25))
+# end = time()
+# print(end-start)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         num_cookies = int(sys.argv[1])
         print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
-            ways=eating_cookies(num_cookies), n=num_cookies))
+            ways=cache_eating_cookies(num_cookies), n=num_cookies))
     else:
         print('Usage: eating_cookies.py [num_cookies]')
